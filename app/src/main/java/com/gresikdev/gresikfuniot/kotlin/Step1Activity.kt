7caee73f2a272a2d65f438a70f09d6173d6daa30
Abdurrouf <roufroufrouf@gmail.com>
2018-10-20 13:01:58 +0700

    Manual Parameter URL

---
+++ app/src/main/java/com/gresikdev/gresikfuniot/kotlin/Step1Activity.kt
@@ -0,0 +1,44 @@
+package com.gresikdev.gresikfuniot.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.gresikdev.gresikfuniot.R
+import kotlinx.android.synthetic.main.activity_step1.*
+import org.jetbrains.anko.doAsync
+import java.net.URL
+
+class Step1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step1)
+
+        btnSubmitBlack?.setOnClickListener {
+            doAsync {
+                val urlArduino = "http://10.37.11.235/genericArgs?R=0&G=0&B=0"
+                URL(urlArduino).readText()
+            }
+        }
+
+        btnSubmitRed?.setOnClickListener {
+            doAsync {
+                val urlArduino = "http://10.37.11.235/genericArgs?R=255&G=0&B=0"
+                URL(urlArduino).readText()
+            }
+        }
+
+        btnSubmitGreen?.setOnClickListener {
+            doAsync {
+                val urlArduino = "http://10.37.11.235/genericArgs?R=0&G=255&B=0"
+                URL(urlArduino).readText()
+            }
+        }
+
+        btnSubmitBlue?.setOnClickListener {
+            doAsync {
+                val urlArduino = "http://10.37.11.235/genericArgs?R=0&G=0&B=255"
+                URL(urlArduino).readText()
+            }
+        }
+    }
+}
